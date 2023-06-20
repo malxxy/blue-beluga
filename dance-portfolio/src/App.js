4b1d3645788dbf5078e56fb68b12e07123210643
malxxy <maliayraguen@gmail.com>
2023-06-19 22:44:58 -0700

    nav bar basic styling

--- dance-portfolio/src/App.js
+++ dance-portfolio/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import Main from './components/pages/Main';
--- dance-portfolio/src/components/Navbar.js
+++ dance-portfolio/src/components/Navbar.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import '/nav.css';
 
 function Navbar() {
   return (
-    <nav className="navbar">
+    <nav>
         <a className='nameTitle' href='/'>Malia Faith</a>
-        <ul>
+        <ul className='navUl'>
             <li className='navItem'><a href='Portfolio'>Portfolio</a></li>
             <li className='navItem'><a href='Resume'>Resume</a></li>
             <li className='navItem'><a href='Contact'>Contact</a></li>
