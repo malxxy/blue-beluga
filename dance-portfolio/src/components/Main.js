844a4c0c93e0c824bc04908a38d9630501cb8ae8
malxxy <maliayraguen@gmail.com>
2023-06-19 22:04:34 -0700

    establish main component outline

---
+++ dance-portfolio/src/components/Main.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Main() {
+  return (
+    <div>
+      <p className="main-page">This is where the main page will go</p>
+    </div>
+  );
+}
+
+export default Main;
